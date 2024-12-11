550d37ae64a4311ffc345b140ace94e71af10803
yui1227 <fred96128@gmail.com>
2024-12-12 00:20:03 +0800

    巨人截圖的過程改成在記憶體內進行

--- cogs/fun.py
+++ cogs/fun.py
@@ -4,6 +4,7 @@ from discord import Interaction, app_commands, File, Object
 import subprocess
 import io
 
+
 class Fun(commands.Cog):
     def __init__(self, bot: YuiDcBot):
         self.bot = bot
@@ -11,9 +12,16 @@ class Fun(commands.Cog):
     @app_commands.command(name="aot-screenshot", description="看巨人現在演到哪裡")
     async def aot_screenshot(self, interaction: Interaction):
         await interaction.response.defer(ephemeral=False)
-        subprocess.run("ffmpeg -y -i $(~/yt-dlp -g g4zpC0WfNx4) -vframes 1 ./aot.jpg",
-                       shell=True, capture_output=True)
-        await interaction.followup.send(file=File("./aot.jpg"), ephemeral=False)
+        proc = subprocess.Popen("""
+ffmpeg -y -i $(~/yt-dlp -g g4zpC0WfNx4) -vframes 1 -f image2pipe -vcodec mjpeg pipe:1
+""", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        image_data, error = proc.communicate()
+        if proc.returncode != 0:
+            print("err")
+            await interaction.followup.send(f"ffmpeg error: {error}", ephemeral=False)
+            return
+        byteio = io.BytesIO(image_data)
+        await interaction.followup.send(file=File(byteio, filename="aot.jpg"), ephemeral=False)
 
 
 async def setup(bot: YuiDcBot):
