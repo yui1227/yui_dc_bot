07302018824475b9b3a32f8a36547ae6e3612fec
yui1227 <fred96128@gmail.com>
2024-12-12 03:00:27 +0800

    使用asyncio版本重寫巨人截圖

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 from yuidcbot import YuiDcBot
 from discord import Interaction, app_commands, File, Object
-import subprocess
+from asyncio import subprocess
 import io
 import json
 from random import choice
@@ -10,18 +10,16 @@ from random import choice
 class Fun(commands.Cog):
     def __init__(self, bot: YuiDcBot):
         self.bot = bot
+        self.aot_ss_cmd = "ffmpeg -y -i $(~/yt-dlp -g g4zpC0WfNx4) -vframes 1 -f image2pipe -vcodec mjpeg pipe:1"
         with open("resources/million_cards.json") as f:
             self.million_card: list[str] = json.load(f)
 
     @app_commands.command(name="aot-screenshot", description="看巨人現在演到哪裡")
     async def aot_screenshot(self, interaction: Interaction):
         await interaction.response.defer(ephemeral=False)
-        proc = subprocess.Popen("""
-ffmpeg -y -i $(~/yt-dlp -g g4zpC0WfNx4) -vframes 1 -f image2pipe -vcodec mjpeg pipe:1
-""", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        image_data, error = proc.communicate()
+        proc = await subprocess.create_subprocess_shell(self.aot_ss_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        image_data, error = await proc.communicate()
         if proc.returncode != 0:
-            print("err")
             await interaction.followup.send(f"ffmpeg error: {error.decode()}", ephemeral=False)
             return
         byteio = io.BytesIO(image_data)
